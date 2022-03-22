905950893ddf3ce68314ef697cc7aa32779c2be7
Monday6984 <ofem302@gmail.com>
2022-03-22 15:16:49 -0700

    Yes

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _puts - prints string
+ * puts2 - print alternating chars of string
  * @str: string
  * Return: void
  */
 
-void _puts(char *str)
+void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
 	}
-
 	_putchar('\n');
 }
